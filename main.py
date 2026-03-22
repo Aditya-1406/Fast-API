086d323dbd9224e68deaaed5f56ade56c38b5778
Aditya-1406 <gargaditya674@gmail.com>
2026-03-22 17:03:29 +0530

    get and post request

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Hello World"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data":"These are the posts"}
+
+@app.post("/posts")
+async def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {'message' : 'Post created'}
